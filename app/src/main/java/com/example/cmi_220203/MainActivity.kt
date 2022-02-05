f2dbacdf606ab645c6c5dab524689cabe234c83a
superick <superick@naver.com>
2022-02-05 21:21:49 +0900

    작은 버튼 토스트 띄워보기

--- app/src/main/java/com/example/cmi_220203/MainActivity.kt
+++ app/src/main/java/com/example/cmi_220203/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cmi_220203
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 //            ctrl + / 도 주석
             Log.d("메인화면로그","클릭용 버튼 눌림")
             Log.e("메인화면로그","에러 관련 로그")
+
+        }
+
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+
 
         }
 
