93f202f9814da39bc348fd195014908d1c1f9dbc
superick <superick@naver.com>
2022-02-05 21:14:31 +0900

    로그 찍어보기

--- app/src/main/java/com/example/cmi_220203/MainActivity.kt
+++ app/src/main/java/com/example/cmi_220203/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.cmi_220203
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+
+            // 주석
+//            ctrl + / 도 주석
+            Log.d("메인화면로그","클릭용 버튼 눌림")
+
+
+
+        }
 
 
     }
